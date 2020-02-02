8f0e38248cebc99c151681d8a211eb3e3b7cf785
John Sizemore <size@Johns-MacBook-Pro.local>
2020-02-01 17:22:49 -0700

    good start

--- src/explore.py
+++ src/explore.py
@@ -4,7 +4,6 @@ import numpy as np
 import datetime as dt 
 from team_dict import team_dict
 
-
 if __name__ == '__main__':
     fname = 'helpers/cols2use.txt'
     cols = []
@@ -16,7 +15,7 @@ if __name__ == '__main__':
     plays['game_date'] = pd.to_datetime(plays['game_date'])
     hist = pd.read_csv('data/spreadspoke_scores.csv')
     hist['schedule_date'] = pd.to_datetime(hist['schedule_date'])
-    m0 = hist['schedule_date'] > plays['game_date'].min()
+    m0 = hist['schedule_date'] >= plays['game_date'].min()
     hist['team_home'] = hist['team_home'].map(team_dict)
     hist['team_away'] = hist['team_away'].map(team_dict)
     hist.rename(columns = {'schedule_date': 'game_date',
@@ -25,3 +24,14 @@ if __name__ == '__main__':
     hist_cols = ['game_date', 'home_team', 'away_team', 'team_favorite_id', 'spread_favorite', 'over_under_line']
     hist_merge = hist[hist_cols][m0].copy()
     plays = pd.merge(plays, hist_merge, how = 'left', on = ['game_date','home_team','away_team'])
+    plays['drive_id'] = plays.apply(lambda row: str(row['game_id']) + str(row['drive']), axis = 1)
+    
+
+    # drive_df = plays.groupby('drive_id').agg({'sp': 'sum',
+    #                                             'field_goal_result': 'sum',
+    #                                             'punt_blocked': 'sum',
+    #                                             'fourth_down_failed': 'sum',
+    #                                             'interception': 'sum',
+    #                                             'fumble_lost': 'sum',
+    #                                             'touchdown': 'sum',
+    #                                             'field_goal_attempt': 'sum'})
