1be8afefeb1969587373f790c3e5c3d907d61cbf
John Sizemore <size@Johns-MacBook-Pro.local>
2020-02-03 18:32:13 -0700

    trying to fix pipe issue

---
+++ src/start_clean.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np 
+import datetime as dt 
+from team_dict import team_dict
+from football_func import *
+import boto3
+
+if __name__ == '__main__':
+    p = pd.read_csv('all_plays_test.csv')
+    print(p.columns)
